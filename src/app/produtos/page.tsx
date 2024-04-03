995cb7d506dc3ea4b0945c96888c0195b002fae7
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2024-04-02 21:37:31 -0300

    fix: url

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -3,13 +3,13 @@ import { Product } from "@/types/product";
 import ProductSection from "./components/section";
 
 async function getData(): Promise<Product[]> {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
   try {
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
     const response = await fetch(`${API_URL}/products`);
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error(`Error fetching data - ${API_URL}/products: `, error);
     throw error;
   }
 }
