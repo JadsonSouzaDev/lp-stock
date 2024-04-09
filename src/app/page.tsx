7256de9433e418830780c3c2933a364188d58984
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2024-04-09 20:02:07 -0300

    feat: reduce logo welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-r from-orange-300 to-orange-200">
       <div className="bg-white py-10 px-8 md:px-12 flex flex-col items-center justify-center rounded-2xl shadow-2xl">
-        <Logo className="w-36 h-36 -mb-10 text-amber-800" />
+        <Logo className="w-32 h-32 -mb-10 text-amber-800" />
         <LogoText className="h-40 w-36 text-amber-800" />
 
         <div className="flex flex-col text-center items-center pb-8 w-full gap-6">
